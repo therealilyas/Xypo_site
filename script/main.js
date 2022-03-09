800373038ad4466ce0ae41810fb0cbebdf2e6e7c
Ilyas Sultanov <ilyas.sultanov2020@gmail.com>
2022-03-09 17:58:30 +0500

    z

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 let menuBtn = document.getElementById("menuBtn");
 let sideNav = document.getElementById("sideNav");
 
+menuBtn.innerText="Menu"    
 sideNav.style.right="-250px";
 menuBtn.onclick = function(){
     if(sideNav.style.right=="-250px"){
